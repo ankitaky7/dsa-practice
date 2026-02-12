cbdbcc8af54dd2fdc045b07dc3545d07c548ef83
ankitaky7 <ankit03061971@gmail.com>
2026-02-12 23:48:22 +0530

    Time: 1 ms (48.92%) | Memory: 10.1 MB (99.38%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int l = 0, r = s.size() - 1;
+        while(l < r){
+            if((s[l] == 'A' || s[l] == 'E' || s[l] == 'I' || s[l] == 'O' || s[l] == 'U' || s[l] == 'a' || s[l] == 'e' || s[l] == 'i' || s[l] == 'o' || s[l] == 'u') && (s[r] == 'A' || s[r] == 'E' || s[r] == 'I' || s[r] == 'O' || s[r] == 'U' || s[r] == 'a' || s[r] == 'e' || s[r] == 'i' || s[r] == 'o' || s[r] == 'u')){
+                swap(s[l], s[r]);
+                l++;
+                r--;
+            }else if(s[l] != 'A' && s[l] != 'E' && s[l] != 'I' && s[l] != 'O' && s[l] != 'U' && s[l] != 'a' && s[l] != 'e' && s[l] != 'i' && s[l] != 'o' && s[l] != 'u'){
+                l++;
+            }else if(s[r] != 'A' && s[r] != 'E' && s[r] != 'I' && s[r] != 'O' && s[r] != 'U' && s[r] != 'a' && s[r] != 'e' && s[r] != 'i' && s[r] != 'o' && s[r] != 'u'){
+                r--;
+            }
+        }
+        return s;
+    }
+};
