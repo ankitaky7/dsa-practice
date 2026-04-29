10c9e1ca012b8e3bc8e9eee49015a29b5f5101cf
ankitaky7 <ankit03061971@gmail.com>
2026-04-29 12:44:36 +0530

    Time: 15 ms (55.04%) | Memory: 18.5 MB (47.77%) - LeetSync

---
+++ dsa-practice/5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        if (n <= 1)
+            return s;
+        // fn to expand from center
+        auto expand_from_center = [&](int left, int right) {
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                left--;
+                right++;
+            }
+            return s.substr(left + 1, right - left - 1); // (right -  1) - (left + 1) - 1
+        };
+        string max_str = s.substr(0, 1);
+        for(int i = 0; i < n - 1; i++){
+            string odd = expand_from_center(i, i); //odd length
+            string even = expand_from_center(i, i + 1); // even length
+
+            if(odd.length() > max_str.length())
+            max_str = odd;
+
+            if(even.length() > max_str.length())
+            max_str = even;
+        }
+        return max_str;
+    }
+};
