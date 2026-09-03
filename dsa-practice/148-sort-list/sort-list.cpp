a7aa782d058d3827992e7d967e42ef05766934d1
ankitaky7 <ankit03061971@gmail.com>
2026-09-03 12:08:45 +0530

    Time: 47 ms (35.47%) | Memory: 75.8 MB (19.85%) - LeetSync

--- dsa-practice/148-sort-list/sort-list.cpp
+++ dsa-practice/148-sort-list/sort-list.cpp
@@ -10,21 +10,49 @@
  */
 class Solution {
 public:
+    ListNode* findMiddle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* merge(ListNode* head1, ListNode* head2){
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+        ListNode* t1 = head1;
+        ListNode* t2 = head2;
+        while(t1 != NULL && t2 != NULL){
+            if(t1->val < t2->val){
+                temp->next = t1;
+                temp = t1;
+                t1 = t1->next;
+            }else{
+                temp->next = t2;
+                temp = t2;
+                t2 = t2->next;
+            }
+        }
+        if(t1){
+            temp->next = t1;
+        }else{
+            temp->next = t2;
+        }
+        return dummyNode->next;
+    }
+
     ListNode* sortList(ListNode* head) {
-        vector<int>num;
-        ListNode* temp = head;
-        while(temp != NULL){
-            num.push_back(temp->val);
-            temp = temp->next;
-        }
-        sort(num.begin(), num.end());
-        int i = 0;
-        temp = head;
-        while(temp != NULL){
-            temp->val = num[i];
-            i++;
-            temp = temp->next;
-        }
-        return head;
+        if(head == NULL || head->next == NULL) return head;
+
+        ListNode* left = head;
+        ListNode* mid = findMiddle(head);
+        ListNode* right = mid->next;
+        mid->next = NULL;
+        left = sortList(left);
+        right = sortList(right);
+        return merge(left, right);
     }
 };
