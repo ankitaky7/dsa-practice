ecc27b6019800b33032f4913e80ee50451f94fdf
ankitaky7 <ankit03061971@gmail.com>
2026-09-08 20:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (35.01%) - LeetSync

--- dsa-practice/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ dsa-practice/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,16 +11,24 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>&res){
-        if(root == NULL) return;
-
-        inorder(root->left, res);
-        res.push_back(root->val);
-        inorder(root->right, res);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>res;
-        inorder(root, res);
-        return res;
+        vector<int>ans;
+        if(root == NULL) return ans;
+        stack<TreeNode*>st;
+        TreeNode* node = root;
+        
+        while(true){
+            if(node != NULL){
+                st.push(node);
+                node = node->left;
+            }else{
+                if(st.empty()) break;
+                node = st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
+        return ans;
     }
 };
