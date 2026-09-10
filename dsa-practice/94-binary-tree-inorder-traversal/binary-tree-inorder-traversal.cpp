5af8d279643541f93d63a38a9fc7de1b4edb519b
ankitaky7 <ankit03061971@gmail.com>
2026-09-10 23:48:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (89.55%) - LeetSync

--- dsa-practice/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ dsa-practice/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -6,19 +6,20 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-        if(root == NULL) return ans;
-        stack<TreeNode*>st;
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        stack<TreeNode*> st;
         TreeNode* node = root;
-        
-        while(true){
-            if(node != NULL){
+        while (true) {
+            if(node != NULL) {
                 st.push(node);
                 node = node->left;
             }else{
