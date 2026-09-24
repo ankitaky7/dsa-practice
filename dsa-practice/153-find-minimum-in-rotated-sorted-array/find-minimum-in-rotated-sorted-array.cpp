bb827d6e09bda5436c77b4933742c8f8b5606304
ankitaky7 <ankit03061971@gmail.com>
2026-09-24 17:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (79.67%) - LeetSync

--- dsa-practice/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ dsa-practice/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -3,9 +3,17 @@ public:
     int findMin(vector<int>& nums) {
         int mini = INT_MAX;
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            mini = min(nums[i], mini);
+        int ans;
+        if(n == 1) {
+            ans = nums[0];
+            return ans;
         }
-        return mini;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                ans = nums[i + 1];
+            }
+        }
+        if(nums[n-1] > nums[0]) ans = nums[0];
+        return ans;
     }
 };
